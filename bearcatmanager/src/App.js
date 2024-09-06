56936517bea845af4dafd12c1dfe40f446a89841
Anthony Jack <79334327+DopeAnt25@users.noreply.github.com>
2024-09-06 15:29:43 -0500

    Added BootStrap import
    Added a Bootstrap import and a nav bar it will not work unless you do npm install  bootstrap but this is just temporary until we get the new CSS imports
    - Anthony Jack
    09/06/2024

--- bearcatmanager/src/App.js
+++ bearcatmanager/src/App.js
@@ -1,16 +1,59 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+// import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
 
 function App() {
   return (
     <div className="App">
+      {/* Bootstrap Navbar */}
+      {/* 
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="#">
+          Bearcat Manager
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">
+                Home <span className="sr-only">(current)</span>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Events
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Tickets
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      */}
 
-        <p>
-        Bearcat Manager!
-        </p>
-
+      {/* Main Content */}
       <header>
-        Event Tickets
+        <p>Bearcat Manager!</p>
+        <h1>Event Tickets</h1>
       </header>
     </div>
   );
