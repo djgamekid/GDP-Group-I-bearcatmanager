59247951f63e74a38c08b9680a07e34f50205ebd
Manikala <156376320+cmanikala@users.noreply.github.com>
2024-09-06 11:46:03 -0500

    Update App.js

--- bearcatmanager/src/App.js
+++ bearcatmanager/src/App.js
@@ -12,6 +12,15 @@ function App() {
       <header>
         Event Tickets
       </header>
+      <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Bearcat Manager!
+        </a>
+
     </div>
   );
 }
