c313a772007665dbe51fbe60f6fdb268c8622bca
Anthony Jack <79334327+DopeAnt25@users.noreply.github.com>
2024-09-11 18:25:20 -0500

    Added a potential QR code generator
    I added a qr code generator currently commented out, also added some sketches for a logo. To get the qr code gen. you have to npm install qrcode.react

--- bearcatmanager/src/tickets.js
+++ bearcatmanager/src/tickets.js
@@ -1,12 +1,37 @@
+import React from "react";
+// import QRCode from "qrcode.react"; //npm install qrcode.react
+
 function App() {
-    return (
-<a
-className="App-link"
-href="https://reactjs.org"
-target="_blank"
-rel="noopener noreferrer"
->
-Back to home page
-</a>
-    )
+  // const qrValue = "https://reactjs.org"; // Predefined value for the QR code
+
+  return (
+    <div className="App">
+      <h1>Pre-loaded QR Code</h1> {/* Heading for the QR code generator */}
+
+      {/* QRCode component that generates the QR code */}
+      {/* 
+      <div style={{ marginTop: '20px' }}>
+        <QRCode
+          value={qrValue}           // The value to be encoded in the QR code
+          size={256}                // Size of the QR code (in pixels)
+          bgColor="#ffffff"         // Background color (white)
+          fgColor="#000000"         // Foreground color (black)
+          level="Q"                 // Error correction level
+          includeMargin={true}      // Adds margin around the QR code
+        />
+      </div>
+      */}
+
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Back to home page
+      </a>
+    </div>
+  );
 }
+
+export default App;
