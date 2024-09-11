ac7faac0bde024c3a6a48a02ef31b8a1dd8bc2b0
Manikala <156376320+cmanikala@users.noreply.github.com>
2024-09-11 17:23:58 -0500

    worked on navbar

--- bearcatmanager/src/CreateEvent.js
+++ bearcatmanager/src/CreateEvent.js
@@ -1,11 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
+// function CreateEvent() {
+//   return (
+//     <div className="CreateEvent">
+//       <header className="CreateEvent-header">
+        
+//         <p>
+//           Edit <code>src/CreateEvent.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="CreateEvent"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           CreateEvent page!
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default CreateEvent;
+
+
+import React from 'react';
+import './App.css';
+ 
 function CreateEvent() {
   return (
     <div className="CreateEvent">
       <header className="CreateEvent-header">
-        
         <p>
           Edit <code>src/CreateEvent.js</code> and save to reload.
         </p>
@@ -21,5 +47,5 @@ function CreateEvent() {
     </div>
   );
 }
-
+ 
 export default CreateEvent;
--- bearcatmanager/src/Login.js
+++ bearcatmanager/src/Login.js
@@ -1,11 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
+// function Login() {
+//   return (
+//     <div className="Login">
+//       <header className="Login-header">
+//         <img src={logo} className="Login-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/Login.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="Login"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Login page!
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default Login;
+
+import React from 'react';
+import './App.css';
+ 
 function Login() {
   return (
     <div className="Login">
       <header className="Login-header">
-        <img src={logo} className="Login-logo" alt="logo" />
         <p>
           Edit <code>src/Login.js</code> and save to reload.
         </p>
@@ -21,5 +46,5 @@ function Login() {
     </div>
   );
 }
-
+ 
 export default Login;
