b7589b6950f7f4c2e38f9bee5919d71b7dc819a8
Darren Ross <78877539+djgamekid@users.noreply.github.com>
2024-09-20 19:33:51 -0500

    Added documentation & useful links for tech stack

--- bearcatmanager/src/App.js
+++ bearcatmanager/src/App.js
@@ -1,25 +1,3 @@
-// import logo from './logo.svg';
-// import { Route, Routes } from "react-router-dom";
-// import './App.css';
-
-// function App() {
-//   return (
-    
-//     <div className="App">
-
-//         <p>
-//         Bearcat Manager!
-//         </p>
-
-//       <header>
-//         Event Tickets
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
